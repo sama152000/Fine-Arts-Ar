866b87697c7ac2b3647d4b1689d4fd4e33649a99
sama152000 <sama.sama152000@gmail.com>
2025-12-08 00:37:36 +0200

    init

---
+++ src/app/core/features/Faculty-of-Fine-Arts/Faculty-of-Fine-Arts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Faculty-of-Fine-Arts',
+  templateUrl: './Faculty-of-Fine-Arts.component.html',
+  styleUrls: ['./Faculty-of-Fine-Arts.component.css']
+})
+export class FacultyOfFineArtsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
